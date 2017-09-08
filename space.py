4dd00fc38d0844d21c040ac44d8853f035af7940
biggykw <big.baby-@hotmail.com>
2017-09-08 14:45:37 +0700

    create ship.py

--- space.py
+++ space.py
@@ -1,27 +1,25 @@
 import arcade
-
+from models import Ship
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
 
 class SpaceGameWindow(arcade.Window):
-    def __init__(self,width,height):
-        super().__init__(width,height)
-
-        arcade.set_background_color(arcade.color.BLACK)
-        
-        self.ship = arcade.Sprite('ship.png')
-        self.ship.set_position(100,100)
 
 
     def on_draw(self):
         arcade.start_render()
         
         self.ship.draw()
+   
+class Ship:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
     def update(self, delta):
-        ship = self.ship
-        if ship.center_y > SCREEN_HEIGHT:
-            ship.center_y = 0
-        ship.set_position(ship.center_x, ship.center_y + 5)
+        if self.y > 600:
+            self.y = 0
+        self.y += 5
 
 if __name__=='__main__':
     window = SpaceGameWindow(SCREEN_WIDTH,SCREEN_HEIGHT)
