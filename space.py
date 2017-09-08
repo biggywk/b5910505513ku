32e80ec815413957b9502b2a71cd9e327a2b0cae
biggykw <big.baby-@hotmail.com>
2017-09-08 13:56:59 +0700

    add code

---
+++ space.py
@@ -0,0 +1,17 @@
+import arcade
+
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+
+class SpaceGameWindow(arcade.Window):
+    def __init__(self,width,height)
+        super().__init__(width,height)
+
+        arcade.set_background_color(arcade.color.BLACK)
+
+    def on+fraw(self):
+        arcade.start_render()
+
+if __name__=='__main__':
+    window = SpaceGameWindow(SCREEN_Width,SCREEN_HEIGHT)
+    arcade.run()
