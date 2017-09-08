9a34adba97e3b8011ee1438845f2639745659174
biggykw <big.baby-@hotmail.com>
2017-09-08 14:59:26 +0700

    fix bug

--- space.py
+++ space.py
@@ -1,26 +1,26 @@
 import arcade
-from models import Ship
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
-
+from ship import Ship
 class SpaceGameWindow(arcade.Window):
+    def __init__(self,width,height):
+        super().__init__(width,height)
+
+        arcade.set_background_color(arcade.color.BLACK)
+        
+        self.ship = Ship(100,100)
+        self.ship_sprite = arcade.Sprite('ship.png')
 
 
     def on_draw(self):
         arcade.start_render()
         
-        self.ship.draw()
-   
-class Ship:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.ship_sprite.draw()
 
     def update(self, delta):
-        if self.y > 600:
-            self.y = 0
-        self.y += 5
-
+        ship = self.ship
+        ship.update(delta)
+        self.ship_sprite.set_position(ship.x, ship.y)
 if __name__=='__main__':
     window = SpaceGameWindow(SCREEN_WIDTH,SCREEN_HEIGHT)
     arcade.run()
